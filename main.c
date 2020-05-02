93f85fb8a01f5cfcec64e9bf162cb4ae3a401687
farzin <farzin.rb70@gmail.com>
2020-05-02 23:48:47 +0430

    add rx interrupt runtime->some bug in clock frequency

--- main.c
+++ main.c
@@ -1,31 +1,4 @@
-/**
-******************************************************************************
-* @file    GPIO_Toggle\main.c
-* @author  MCD Application Team
-* @version V2.0.4
-* @date    26-April-2018
-* @brief   This file contains the main function for GPIO Toggle example.
-******************************************************************************
-* @attention
-*
-* <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-*
-* Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-* You may not use this file except in compliance with the License.
-* You may obtain a copy of the License at:
-*
-*        http://www.st.com/software_license_agreement_liberty_v2
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-******************************************************************************
-*/
-
-/* Includes ------------------------------------------------------------------*/
+#define STM8S003
 
 #include "stm8s.h"
 #include "globalVar.h"
@@ -33,11 +6,6 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
-* @addtogroup GPIO_Toggle
-* @{
-*/
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -48,15 +16,17 @@ uint8_t text[100];
 uint16_t* readADC;
 uint16_t readedTemp,readedWaterLvl;
 uint8_t setTemp,tempTelorance,setWaterLvl,waterLvlTelorance;
+
+volatile uint8_t rxBuffer[100],rxCounter , recivePacketFlag;
 /* Private function prototypes -----------------------------------------------*/
 void Delay (uint16_t nCount);
 static void ADC_Config(void);
 static void GPIO_Config(void);
 static void TIM2_Config(void);
-void UART1_setup(void);
+static void ClOCK_Config(void);
+static void UART1_setup(void);
 uint16_t* readAllADC(void);
 void SendData(uint8_t *data, unsigned int len);
-
 void LoopFunction(void);
 
 /* Private functions ---------------------------------------------------------*/
@@ -69,28 +39,21 @@ void LoopFunction(void);
 */
 void main(void)
 {
-  CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
-  CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
-  CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER4, ENABLE);
-  CLK_PeripheralClockConfig(CLK_PERIPHERAL_ADC, ENABLE);
-  CLK_PeripheralClockConfig(CLK_PERIPHERAL_UART1, ENABLE);
+  ClOCK_Config();
   GPIO_Config();
   ADC_Config();
   UART1_setup();
   readADC = calloc(6,sizeof(unsigned char));
+  enableInterrupts();
   while (1)
   {
     LoopFunction();
-    
-    
-    
-    
-    
-//    readADC = readAllADC();
-//    sprintf((char*)text,"pot:%u,%u,%u\r\n",readADC[0],readADC[1]);
-//    SendData(text,strlen((const char*)text));
   }
 }
+
+
+
+
 
 void LoopFunction(void){
   //raed temp and water level sensor 
@@ -103,7 +66,8 @@ void LoopFunction(void){
   }else if (readedTemp > setTemp + tempTelorance){
     OFF(HEATER);
   }
-
+  
+  
   //calculate water level from sensors value 
   readedWaterLvl = readADC[SENSOR_WATER_LVL_CH]/102;
   //water level control runtime 
@@ -112,8 +76,14 @@ void LoopFunction(void){
   }else if (readedWaterLvl > setWaterLvl + waterLvlTelorance){
     OFF(WATER_VALVE);
   }
-  
-  
+  if(recivePacketFlag==1){
+    SendData((uint8_t *)rxBuffer,(unsigned int)rxCounter);
+    rxCounter=0;
+    recivePacketFlag = 0;
+  }
+  //send water level and temp to esp12
+  //sprintf((char*)text,"pot:%u,%u,%u\r\n",readADC[0],readADC[1]);
+  //SendData(text,strlen((const char*)text));
 }
 
 
@@ -144,6 +114,22 @@ uint16_t* readAllADC(void){
 }
 
 
+static void ClOCK_Config(void){
+  CLK_HSECmd(DISABLE);
+  CLK_LSICmd(DISABLE);
+  CLK_HSICmd(ENABLE);
+                                
+  CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+  CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
+                                
+
+  
+  CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER4, ENABLE);
+  CLK_PeripheralClockConfig(CLK_PERIPHERAL_ADC, ENABLE);
+  CLK_PeripheralClockConfig(CLK_PERIPHERAL_UART1, ENABLE);
+}
+
+
 static void GPIO_Config(void)
 {
   /* Initialize I/Os in Output Mode */
@@ -155,17 +141,21 @@ static void GPIO_Config(void)
   GPIO_Init(LIGHT, GPIO_MODE_OUT_PP_LOW_FAST);
 }
 
-void UART1_setup(void)
+static void UART1_setup(void)
 {
+  GPIO_Init(GPIOD, GPIO_PIN_5, GPIO_MODE_OUT_PP_HIGH_FAST);
+  GPIO_Init(GPIOD, GPIO_PIN_6, GPIO_MODE_IN_FL_NO_IT);
   UART1_DeInit();
   
   UART1_Init(9600,
              UART1_WORDLENGTH_8D,
-             UART1_STOPBITS_1, 
-             UART1_PARITY_NO, 
-             UART1_SYNCMODE_CLOCK_DISABLE, 
-             UART1_MODE_TX_ENABLE);
+             UART1_STOPBITS_1,
+             UART1_PARITY_NO,
+             UART1_SYNCMODE_CLOCK_DISABLE,
+             UART1_MODE_TXRX_ENABLE);
+  UART1_ITConfig(UART1_IT_RXNE,ENABLE);
   UART1_Cmd(ENABLE);
+  
 }
 
 
@@ -174,29 +164,21 @@ static void ADC_Config()
   GPIO_Init(SENSOR_WATER_LVL, GPIO_MODE_IN_FL_NO_IT);
   GPIO_Init(SENSOR_TEMP, GPIO_MODE_IN_FL_NO_IT);
   ADC1_Init(ADC1_CONVERSIONMODE_CONTINUOUS,
-                 ADC1_CHANNEL_2,
-                 ADC1_PRESSEL_FCPU_D18,
-                 ADC1_EXTTRIG_GPIO,
-                 DISABLE,
-                 ADC1_ALIGN_RIGHT,
-                 ADC1_SCHMITTTRIG_CHANNEL2,
-                 DISABLE);
+            ADC1_CHANNEL_2,
+            ADC1_PRESSEL_FCPU_D18,
+            ADC1_EXTTRIG_GPIO,
+            DISABLE,
+            ADC1_ALIGN_RIGHT,
+            ADC1_SCHMITTTRIG_CHANNEL2,
+            DISABLE);
   ADC1_Init(ADC1_CONVERSIONMODE_CONTINUOUS,
-                 ADC1_CHANNEL_4,
-                 ADC1_PRESSEL_FCPU_D18,
-                 ADC1_EXTTRIG_GPIO,
-                 DISABLE,
-                 ADC1_ALIGN_RIGHT,
-                 ADC1_SCHMITTTRIG_CHANNEL4,
-                 DISABLE);
-  ADC1_Init(ADC1_CONVERSIONMODE_CONTINUOUS,
-                 ADC1_CHANNEL_6,
-                 ADC1_PRESSEL_FCPU_D18,
-                 ADC1_EXTTRIG_GPIO,
-                 DISABLE,
-                 ADC1_ALIGN_RIGHT,
-                 ADC1_SCHMITTTRIG_CHANNEL6,
-                 DISABLE);
+            ADC1_CHANNEL_3,
+            ADC1_PRESSEL_FCPU_D18,
+            ADC1_EXTTRIG_GPIO,
+            DISABLE,
+            ADC1_ALIGN_RIGHT,
+            ADC1_SCHMITTTRIG_CHANNEL3,
+            DISABLE);
   ADC1_Cmd(ENABLE);
 }
 
--- stm8s_it.h
+++ stm8s_it.h
@@ -28,7 +28,6 @@
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM8S_IT_H
 #define __STM8S_IT_H
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
 
